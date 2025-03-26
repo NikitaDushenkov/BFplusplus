48a3c385d85cd6fe2116df0d3d58d0b7b9fb018e
NikitaDushenkov <130363428+NikitaDushenkov@users.noreply.github.com>
2025-03-26 19:57:28 +0300

    updated error handling

--- interpreter.py
+++ interpreter.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import requests
 
 class BrainfuckInterpreter:
     def __init__(self, code, input_data=""):
@@ -11,7 +12,14 @@ class BrainfuckInterpreter:
         self.input_pointer = 0
         self.bracket_map = self.build_bracket_map()
         self.modules = {}
-
+    
+    def error(self, message):
+        r = requests.get('https://bofh-api.bombeck.io/v1/excuses/random/')
+        if r.status_code != 200:
+            raise SyntaxError("no internet sad" + message)
+        else:
+            raise SyntaxError(r.json()[0]['quote'] + " " + message)
+    
     def build_bracket_map(self):
         stack = []
         bracket_map = {}
@@ -20,12 +28,12 @@ class BrainfuckInterpreter:
                 stack.append(i)
             elif char == "]":
                 if not stack:
-                    raise SyntaxError("Unmatched closing bracket at position {}".format(i))
+                    self.error("Unmatched closing bracket at position {}".format(i))
                 start = stack.pop()
                 bracket_map[start] = i
                 bracket_map[i] = start
         if stack:
-            raise SyntaxError("Unmatched opening bracket at position {}".format(stack.pop()))
+            self.error("Unmatched opening bracket at position {}".format(stack.pop()))
         return bracket_map
 
     def run(self):
@@ -59,16 +67,16 @@ class BrainfuckInterpreter:
             elif command == "!":
                 self.data[self.data_pointer] = 0
             elif command == '{':
-                end_index = code.find('}', self.code_pointer)
+                end_index = self.code.find('}', self.code_pointer)
                 if end_index == -1:
-                    raise SyntaxError("Unmatched '{'")
-                module_name = code[self.code_pointer + 1:end_index]
+                    self.error("Unmatched '{'")
+                module_name = self.code[self.code_pointer + 1:end_index]
                 if module_name.isnumeric():
                     module_name = int(module_name)
                     if module_name in self.modules:
                         self.data[self.data_pointer] = ord(self.run_module(module_name))
                     else:
-                        raise ValueError(f"Module with ID {module_name} not found.")
+                        self.error(f"Module with ID {module_name} not found.")
                 else:
                     self.load_module(module_name, self.data[self.data_pointer])
                     self.code_pointer = end_index
@@ -80,13 +88,14 @@ class BrainfuckInterpreter:
                 module_code = module_file.read()
                 self.modules[module_index] = module_code
         except FileNotFoundError:
-            raise FileNotFoundError(f"Module '{module_name}' not found.")
+            self.error(f"Module '{module_name}' not found.")
 
     def run_module(self, module_id):
         module_code = self.modules[module_id]
         input_char = chr(self.data[self.data_pointer])
         sub_interpreter = BrainfuckInterpreter(module_code, input_char)
         return sub_interpreter.run()
+        
 
 
 
